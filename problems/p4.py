1e89e31d667d5a8b162cc8c89c8645d181dcd42e
Venkata Ramana C <arjunchitturi@gmail.com>
2012-01-09 22:25:07 +0530

    avoid re-calculation

--- problems/p4.py
+++ problems/p4.py
@@ -9,7 +9,7 @@ for a in xrange(999, 100, -1):
         maybe_palindrome = a * b
         #check if larger number than palindrome
         if maybe_palindrome > palindrome:
-            s = str(a * b)
+            s = str(maybe_palindrome)
             if s == s[::-1]:
                 palindrome = maybe_palindrome
 print palindrome
