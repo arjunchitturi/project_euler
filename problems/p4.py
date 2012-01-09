d2b88824ec78988f00f2da99f98bf0ec4dabd5d8
Venkata Ramana C <arjunchitturi@gmail.com>
2012-01-09 20:52:29 +0530

    largest palindrome

--- problems/p4.py
+++ problems/p4.py
@@ -7,12 +7,9 @@ palindrome = 0
 for a in xrange(999, 100, -1):
     for b in xrange(a, 100, -1):
         maybe_palindrome = a * b
-        #compare if new number is greater than palindrome
-        #before checking for palindrome
+        #check if larger number than palindrome
         if maybe_palindrome > palindrome:
             s = str(a * b)
             if s == s[::-1]:
                 palindrome = maybe_palindrome
-
 print palindrome
-
