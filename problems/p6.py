b6716dfbe9d8ed3f939e980b47752929373ad466
Venkata Ramana C <arjunchitturi@gmail.com>
2012-01-10 11:31:04 +0530

    problem 6

---
+++ problems/p6.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python
+'''
+http://projecteuler.net/problem=6
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+Answer: 25164150
+'''
+r = xrange(1, 101)
+a = sum(r)
+print a * a - sum(i*i for i in r)
